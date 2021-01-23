250e53e8c8e0a199ec588049275ca37656aada7f
ahron levi <aharon.levi@amobee.com>
2021-01-23 23:41:21 +0200

    Delete log.py

--- src/log.py
+++
@@ -1,8 +0,0 @@
-import logging
-import logging.handlers
-
-def setup_config() -> None:
-  logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
-
-def get_logger(name="root"):
-  logging.getLogger(name)
