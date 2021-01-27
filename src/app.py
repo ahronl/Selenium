cb20d206681e2c0e5d0c0c7a8db50e5e9dae42ab
ahronl <aharon.levi@amobee.com>
2021-01-28 00:54:07 +0200

    read config file yaml

--- src/app.py
+++ src/app.py
@@ -1,13 +1,21 @@
 from log import set_logging_config, get_logger
 from driver_factory import DriverFactory
+from pathlib import Path
+
+import yaml
+
+def read_conf():
+    path = (Path(__file__).parent / "../etc/config.yml").resolve()
+    with open(path) as file:
+        conf = yaml.load(file, Loader=yaml.FullLoader)
+        return conf
 
 def app() -> None:
-    
     set_logging_config()
-
     get_logger().info("starting....")
 
-    driver_factory = DriverFactory()
+    config = read_conf()
+    driver_factory = DriverFactory(config)
     driver = driver_factory.create('chrome_remote')
     driver.maximize_window()
     driver.get("https://www.python.org")
--- src/driver_factory.py
+++ src/driver_factory.py
@@ -2,17 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class DriverFactory:
+    
+    def __init__(self, config):
+        self._config = config
+
     def create(self, name):
         if name == 'chrome':
             return create_chrome_driver
         elif name == 'chrome_remote':
-            return create_remote_chrome_driver()
+            return create_remote_chrome_driver(self._config)
         elif name == 'firefox':
             return create_firefox_driver()
         elif name == 'firefox_remote':
             return create_remote_firefox_driver()
-
-
+    
 def create_chrome_options() -> None:
     chrome_options = webdriver.chrome.options.Options()
     chrome_options.experimental_options["prefs"] = {}
@@ -22,12 +25,13 @@ def create_chrome_driver():
     chrome_options = create_chrome_options()
     return webdriver.Chrome(options=chrome_options)
 
-def create_remote_chrome_driver():
-    driver = webdriver.Remote(command_executor='http://host.docker.internal:4444/wd/hub',options=create_chrome_options())
+def create_remote_chrome_driver(config):
+    host = config['web_driver']['remote']['uri']
+    driver = webdriver.Remote(command_executor=f"{host}/wd/hub",options=create_chrome_options())
     return driver
-
+    
 def create_firefox_driver():
     raise NotImplementedError
-
+    
 def create_remote_firefox_driver():
     raise NotImplementedError
