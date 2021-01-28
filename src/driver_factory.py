3f9b33f8d21caa0db41a379aafe4fc357d54281a
ahronl <aharon.levi@amobee.com>
2021-01-28 14:56:22 +0200

    clean driver factory

--- src/driver_factory.py
+++ src/driver_factory.py
@@ -1,37 +1,36 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 class DriverFactory:
     
-    def __init__(self, config):
-        self._config = config
+    def __init__(self, conf):
+        self._config = conf
 
     def create(self, name):
         if name == 'chrome':
-            return create_chrome_driver
+            return self.create_chrome_driver()
         elif name == 'chrome_remote':
-            return create_remote_chrome_driver(self._config)
+            return self.create_remote_chrome_driver()
         elif name == 'firefox':
-            return create_firefox_driver()
+            return self.create_firefox_driver()
         elif name == 'firefox_remote':
-            return create_remote_firefox_driver()
+            return self.create_remote_firefox_driver()
     
-def create_chrome_options() -> None:
-    chrome_options = webdriver.chrome.options.Options()
-    chrome_options.experimental_options["prefs"] = {}
-    return chrome_options
+    def create_chrome_options(self):
+        chrome_options = webdriver.chrome.options.Options()
+        chrome_options.experimental_options["prefs"] = {}
+        return chrome_options
 
-def create_chrome_driver():
-    chrome_options = create_chrome_options()
-    return webdriver.Chrome(options=chrome_options)
+    def create_chrome_driver(self):
+        chrome_options = create_chrome_options()
+        return webdriver.Chrome(options=chrome_options)
 
-def create_remote_chrome_driver(config):
-    host = config['web_driver']['remote']['uri']
-    driver = webdriver.Remote(command_executor=f"{host}/wd/hub",options=create_chrome_options())
-    return driver
+    def create_remote_chrome_driver(self):
+        host = self._config['web_driver']['remote']['uri']
+        driver = webdriver.Remote(command_executor=f"{host}/wd/hub",options=self.create_chrome_options())
+        return driver
     
-def create_firefox_driver():
-    raise NotImplementedError
+    def create_firefox_driver(self):
+        raise NotImplementedError
     
-def create_remote_firefox_driver():
-    raise NotImplementedError
+    def create_remote_firefox_driver(self):
+        raise NotImplementedError
